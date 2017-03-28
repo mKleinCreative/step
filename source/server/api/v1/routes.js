95f22564c1c0b4c11865f27bc8c4d307b7f23b4c
Ian Deans <ideans715@gmail.com>
2017-03-28 11:32:33 -0700

    Auth WIP

--- source/server/api/v1/routes.js
+++ source/server/api/v1/routes.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import passport from 'passport'
 import {
   handleNewCouldDo,
   handleEditCouldDo,
@@ -14,6 +15,21 @@ import {
 
 const router = express()
 
+router.get(
+  '/auth/google',
+  passport.authenticate( 'google', { scope:
+  [
+    'https://www.googleapis.com/auth/plus.login',
+    'https://www.googleapis.com/auth/plus.profile.emails.read'
+  ]
+}))
+
+router.get( '/google/auth/callback',
+  passport.authenticate( 'google', { failureRedirect: '/' } ),
+  ( request, response ) => {
+    response.status(200).json({ message: 'shit failed' })
+  })
+
 router.post( '/could-do/new', handleNewCouldDo )
 router.post( '/could-do/edit/:id', handleEditCouldDo )
 router.post( '/could-do/delete/:id', handleDeleteCouldDo )
@@ -23,6 +39,6 @@ router.post( '/project/new', handleNewProject )
 router.post( '/project/edit/:id', handleEditProject )
 router.post( '/project/delete/:id', handleDeleteProject )
 
-router.get( '/user/:id/projects', handleGetProjectsByUserId)
+router.get( '/user/:id/projects', handleGetProjectsByUserId )
 
 export default router
--- source/server/server.js
+++ source/server/server.js
@@ -6,14 +6,26 @@ import path from 'path'
 import chalk from 'chalk'
 import bodyParser from 'body-parser'
 import router from './api/v1/routes'
+import passport from 'passport'
+import StrategeryDood from './oauth'
+
+passport.serializeUser( ( user, done ) => done( null, user ) )
+passport.deserializeUser( ( obj, done ) => done( null, obj ) )
+
+//HACK: name change?
+passport.use( StrategeryDood )
+
 
 const server = express()
 
 server.set( 'port', process.env.PORT || '1337' )
+server.use( passport.initialize() )
+
 server.use( express.static( 'public' ))
 
 server.use( bodyParser.json() )
 server.use( router )
+
 
 server.get('*', ( request, response ) =>
   response.sendFile( path.join( __dirname, '/../../public/index.html' ) )
